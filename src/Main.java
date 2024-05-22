b363162356f22760d49cde74d3fee5a103ae5a30
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-05-22 22:49:38 +0300

    created two list for nums && list for words.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>(List.of(1, 2, 4, 6, 8, 6, 11, 13, 75, 24, 56, 3));
+        List<String> words = new ArrayList<>(List.of("apple", "pineapple", "orange", "orange", "pineapple", "banana", "orange"));
+    }
+}
